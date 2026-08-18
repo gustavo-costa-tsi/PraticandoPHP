2472d7042eacacd0dc556f73fe11e727975ae784
Gustavo de Souza Costa <souzagustavo903@gmail.comm>
2026-08-18 03:29:53 -0300

    style: corrigindo formatação

--- desafios/desafio2/desafio2.php
+++ desafios/desafio2/desafio2.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,10 +14,11 @@
 </header>
 
   <main>
-    
+
     <?php 
      echo rand(1, 100);
     ?>
+    
   <p><a href="javascript:history.go(-1)">Voltar para página anterior</a></p>
   </main>
 </body>
